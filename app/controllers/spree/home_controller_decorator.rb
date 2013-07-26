9abf0757c1c67bddb7f8f9c41f485cf67b60078e
S. Tunji Turner <vision@firstsightmedia.com>
2013-07-26 08:31:28 -0400

    try code

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -17,7 +17,7 @@ Spree::HomeController.class_eval do
     @searcher = Spree::Config.searcher_class.new(params)
     @searcher.current_user = try_spree_current_user
     @searcher.current_currency = current_currency
-    @products = @searcher.retrieve_products
+    @products = @searcher.retrieve_products.order("spree_products.created_at DESC")
     respond_with(@products)
     
   end
